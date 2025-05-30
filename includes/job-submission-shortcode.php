3937d54c54559b1e33b108d97407351415f481c5
Kat-Gumerov <katrinagumerov24@gmail.com>
2025-05-30 19:10:27 +0000

    directing it to my personal email

--- includes/job-submission-shortcode.php
+++ includes/job-submission-shortcode.php
@@ -223,7 +223,7 @@ function ap_handle_job_submission() {
     update_post_meta( $post_id, 'is_affiliate', $is_aff );
 
     // f) Email with “Edit in WordPress” button
-    $director_email = 'affiliates@creol.ucf.edu';
+    $director_email = 'katrina.gumerov@ucf.edu';
     $edit_url       = admin_url( "post.php?post={$post_id}&action=edit" );
 
     $subject = 'New Job Submission Pending Review';
